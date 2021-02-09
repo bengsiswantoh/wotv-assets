1adb59748a35e14202d9f866422ae3a1d4f60cc2
K0lb3 <rudolf.kolbe@st.ovgu.de>
2021-02-09 22:37:41 +0100

    QooApp apk download fix

--- lib/version.py
+++ lib/version.py
@@ -1,6 +1,7 @@
 import re
 import http.client
-import urllib.request
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
 import os
 import zipfile
 import io
@@ -43,18 +44,56 @@ def update_version(id, path = ""):
     return HOST, VERSION
 
 def download_QooApp_apk(apk):
-    con = http.client.HTTPSConnection('api.qoo-app.com')
-    con.connect()
-    con.request("GET", f'/v6/apps/{apk}/download')
-    res = con.getresponse()
-    con.close()
-    download_url = res.headers['Location']
-    data =  download_with_bar(download_url)
+    query = urlencode({
+        "supported_abis"	:	"x86,armeabi-v7a,armeabi",
+        "device"	:	"beyond1q",
+        "base_apk_version"	:	"0",
+        "locale"	:	"en",
+        "opengl"	:	"196609",
+        "rooted"	:	"true",
+        "screen"	:	"900,1600",
+        "userId"	:	"58009610",
+        "device_model"	:	"SM-G973N",
+        "sdk_version"	:	"22",
+        "base_apk_md5"	:	"null",
+        "user_id"	:	"58009610",
+        "version_code"	:	"317",
+        "version_name"	:	"8.1.7",
+        "os"	:	"android+5.1.1",
+        "adid"	:	"64d6639f-55fe-4a86-86fa-a5ea31b2adc7",
+        "type"	:	"app",
+        "uuid"	:	"7e86a27e-db1c-4072-a5cc-e4b9b08e0672",
+        "device_id"	:	"80e65e35094bedcc",
+        "package_id"	:	"com.qooapp.qoohelper",
+        "otome"	:	"0",
+        "token"	:	"049b1432e342d571a01235ec1d6a91f61fc1db2e",
+        "android_id"	:	"80e65e35094bedcc",
+        "sa_distinct_id"	:	"80e65e35094bedcc",
+        "patch_code"	:	"48",
+        "density"	:	"320",
+        "system_locale"	:	"en_DE",
+    })
+    res = urlopen(Request(
+        url = f"https://api.qoo-app.com/v6/apps/{apk}/download?{query}",
+        headers = {
+            "accept-encoding": "gzip",
+            "user-agent": "QooApp 8.1.7",
+            "device-id": "80e65e35094bedcc"
+        },
+        method = "GET"
+    ))
+    # con = http.client.HTTPSConnection('api.qoo-app.com')
+    # con.connect()
+    # con.request("GET", f'/v6/apps/{apk}/download?supported_abis=x86,Carmeabi-v7a,Carmeab')
+    # res = con.getresponse()
+    # con.close()
+    # download_url = res.headers['Location']
+    data =  download_with_bar(res.url)
     return data
 
 def download_with_bar(url):
     print(url)
-    resp = urllib.request.urlopen(url)
+    resp = urlopen(url)
     length = resp.getheader('content-length')
     if length:
         length = int(length)
